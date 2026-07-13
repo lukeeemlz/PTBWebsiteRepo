8779450d4d070823696e5e17fd16775d2689dd3c
paauulaamlz <paukaveh@gmail.com>
2026-07-13 13:00:49 +0200

    july 13 noon part 3

--- PTBRepo/PTBWebsite/java/movingtext.js
+++ PTBRepo/PTBWebsite/java/movingtext.js
@@ -1,1 +1,21 @@
-import javax.swing
+function makeLogoClickable() {
+
+    var logo = document.querySelector(".ptblogo");
+
+    if (logo) {
+        console.log("✅ Logo found:", logo);
+
+
+        logo.style.cursor = "pointer";
+
+
+        logo.addEventListener("click", function () {
+            window.location.href = "index.html";
+        });
+    } else {
+        setTimeout(makeLogoClickable, 300);
+    }
+}
+
+
+makeLogoClickable();
